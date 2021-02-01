3862f9095e5fcc5d2c9cc0c7f5f388f44aaedb4a
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2021-02-01 21:18:25 +0200

    Make config client reloadable

--- config-client/src/main/java/demo/config/client/ConfigClientController.java
+++ config-client/src/main/java/demo/config/client/ConfigClientController.java
@@ -2,6 +2,7 @@ package demo.config.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,10 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 
+/**
+ * {@link RefreshScope} is needed to make {@link Value} reloadable with /actuator/refresh
+ */
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
