3bcdd89e3cbb3adbedc0cb5b81b6deb8227d98c4
Anatolii <anatolii.kryvda@gmail.com>
2021-04-04 10:41:33 +0300

    Config client: Discovery First Bootstrap

---
+++ config-client-discovery/src/main/java/demo/ConfigClientDiscoveryController.java
@@ -0,0 +1,30 @@
+package demo;
+
+import demo.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+public class ConfigClientDiscoveryController {
+
+    @Autowired
+    private Config config;
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${demo.value}")
+    private String value;
+
+    @GetMapping
+    public List<String> get() {
+        return asList(config.getValue(), environment.getProperty("demo.value"), value);
+    }
+}
