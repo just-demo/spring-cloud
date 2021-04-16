adb46bf2caa12e8c79da9594f5f557e028d6fd90
Anatolii <anatolii.kryvda@gmail.com>
2021-04-16 13:00:51 +0300

    Rename config file for consistency

--- gateway/src/main/java/demo/GatewayConfiguration.java
+++ gateway/src/main/java/demo/GatewayConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GatewayConfig {
+public class GatewayConfiguration {
     @Bean
     public RouteLocator demoLocator(RouteLocatorBuilder builder) {
         return builder.routes()
